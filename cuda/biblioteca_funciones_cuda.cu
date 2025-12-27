0aac071e9fddd9ae9b7352310d61ffa0f94ed649
jonetxe13 <jonetxe.sasia13@gmail.com>
2025-12-27 11:40:39 +0100

    knn cuda hecho, mirar a ver si se puede mejorar algo, con 1000 es peor que en serie pero con 10000 es mucho mejor

--- cuda/biblioteca_funciones_cuda.cu
+++ cuda/biblioteca_funciones_cuda.cu
@@ -1,169 +1,229 @@
-#include <math.h>
-#include <string.h>
-#include <stdio.h>
 #include "biblioteca_funciones.h"
+#include <cuda_device_runtime_api.h>
+#include <driver_types.h>
+#include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <texture_types.h>
 
 //-------------------
 // COMPARTIDAS:
 //-------------------
 
-double dot_product(float* a, float* b, int size) {
-	double result = 0;
-	for (int i = 0; i < size; i++) {
-		result += a[i] * b[i];
-	}
-	return result;
+__device__ double dot_product(float *a, float *b, int size) {
+  double result = 0;
+  for (int i = 0; i < size; i++) {
+    result += a[i] * b[i];
+  }
+  return result;
 }
 
-float magnitude(float* vec, int size) {
-	float sum = 0;
-	for (int i = 0; i < size; i++) {
-		sum += vec[i] * vec[i];
-	}
-	return sqrt(sum);
+__device__ float magnitude(float *vec, int size) {
+  float sum = 0;
+  for (int i = 0; i < size; i++) {
+    sum += vec[i] * vec[i];
+  }
+  return sqrt(sum);
 }
 
-float cosine_similarity(float* vec1, float* vec2, int size) {
-	float mag1, mag2;
+__device__ float cosine_similarity(float *vec1, float *vec2, int size) {
+  float mag1, mag2;
 
-	mag1 = magnitude(vec1, size);
-	mag2 = magnitude(vec2, size);
-	return dot_product(vec1, vec2, size) / (mag1 * mag2);
+  mag1 = magnitude(vec1, size);
+  mag2 = magnitude(vec2, size);
+  return dot_product(vec1, vec2, size) / (mag1 * mag2);
 }
 
 //-------------------
 // Analogy:
 //-------------------
 
-int word2ind(char* word, char** dictionary, int numwords) {
-    for (int i = 0; i < numwords; i++) {
-        if (strcmp(word, dictionary[i]) == 0) {
-            return i;
-        }
+int word2ind(char *word, char **dictionary, int numwords) {
+  for (int i = 0; i < numwords; i++) {
+    if (strcmp(word, dictionary[i]) == 0) {
+      return i;
     }
-    return -1;  // if the word is not found
+  }
+  return -1; // if the word is not found
 }
 
 // Función para calcular la analogía
-//no creo que merezca la pena ser paralelizada ya que es muy pequepa
-void perform_analogy(float *words, int idx1, int idx2, int idx3, float *result_vector) {
-	int i;
-	for(i=0;i<EMB_SIZE;i++){
-		result_vector[i]=words[idx1*EMB_SIZE+i]-words[idx2*EMB_SIZE+i]+words[idx3*EMB_SIZE+i];
-	}
-	return;
+// no creo que merezca la pena ser paralelizada ya que es muy pequepa
+void perform_analogy(float *words, int idx1, int idx2, int idx3,
+                     float *result_vector) {
+  int i;
+  for (i = 0; i < EMB_SIZE; i++) {
+    result_vector[i] = words[idx1 * EMB_SIZE + i] - words[idx2 * EMB_SIZE + i] +
+                       words[idx3 * EMB_SIZE + i];
+  }
+  return;
 }
 
-// Función para encontrar la palabra más cercana al vector resultante
-void find_closest_word(float *result_vector, float *words, int numWords, int idx1, int idx2, int idx3, int *closest_word_idx, float *max_similarity,int numBloques,int numHilos) {
-	int i,j;
-	*max_similarity = 0.0;
-	*closest_word_idx = -1;	//copiamos el nuevo indice	
-	float *maxBloques = (float*) calloc(numBloques, sizeof(float));
-	int * indices =(int*)malloc(sizeof(int)*numBloques);
-	int *cu_ind;
-	float * cu_max,*cu_words,*cu_vector_res;
-	//pasar los datos a el device:
-	//TODO
-	//crear vector maxBloques 
-	cudaMalloc(&cu_max,numBloques*sizeof(float));
-	//crear y pasar vector indicesBloques
-	cudaMalloc(&cu_ind,numBloques*sizeof(int));
-	//crear y pasar result_vector
-	cudaMalloc(&cu_vector_res, EMB_SIZE*sizeof(float)); 
-	cudaMemcpy (cu_vector_res,result_vector,EMB_SIZE*sizeof(float), cudaMemcpyHostToDevice);
-	//crear y pasar words 
-	cudaMalloc(&cu_words,numWords*EMB_SIZE*sizeof(float));
-	cudaMemcpy (cu_words,words, numWords*EMB_SIZE*sizeof(float), cudaMemcpyHostToDevice);
-
-	// llamada a kernel:
-	reduc_analogy<<<numBloques, numHilos>>>(idx1, idx2, idx3, numWords, cu_ind, cu_max, cu_words, cu_vector_res);
-reduc_analogy(int idx1,int idx2,int idx3,int numWords,int * indicesBloques, float * maximosBloques,float * words,float * result_vector)
-	//traerse los resultados del kernel:
-	cudaMemcpy(maxBloques, cu_max, numBloques*sizeof(float), cudaMemcpyDeviceToHost);
-	cudaMemcpy(indices, cu_ind, numBloques*sizeof(int), cudaMemcpyDeviceToHost);
-	//procesar reslutados:
-	*max_similarity = -1.0;
-	for(i=0;i<numBloques;i++){
-		if (*max_similarity<maxBloques[i]){
-			*max_similarity = maxBloques[i];	
-			*closest_word_idx = indices[i];
-		}
-	}
-
-	//TODO algun free mas?
-	free(maxBloques);  
-	free(indices);
-	cudaFree(cu_max);
-	cudaFree(cu_ind);
-	cudaFree(cu_vector_res);
-	cudaFree(cu_words);
-	return;
-}
-
-__global__ void reduc_analogy(int idx1,int idx2,int idx3,int numWords,int * indicesBloques, int * maximosBloques,float * words,float * result_vector)
-{
-	int tid = threadIdx.x;
-	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	int i;
-	int stride=blockDim.x*gridDim.x;
-	int indice;
-	float simil;
-	float maxSimil=-1;
-	__shared__ int indices[blockDim.x];
-	__shared__ float maximos[blockDim.x];
-	for (i = idx;i<numWords;i+=stride){
-		simil = cosine_similarity(result_vector,&words[i*EMB_SIZE],EMB_SIZE);
-		if (simil>maxSimil&& i !=idx1&& i !=idx2&& i !=idx3){
-			maxSimil = simil;
-			indice = i;
-		}
-	}
-	indices[tid]=indice;
-	maximos[tid]=maxSimil;	
-	//ahora tenemos los mas grandes entre los que le ha tocado procesar a este thread
-	__syncthreads ();  
-	for (stride=1; stride<blockDim.x; stride*=2)
-	{
-		if ((tid % (2*stride)) == 0)
-		{
-			if(maximos[tid+stride]>maximos[tid]){
-				maximos[tid]=maximos[tid+stride];
-				indices[tid]=indices[tid+stride];
-			}
-			//esto no hace obviamente falta obviamente, pero lo dejo porque se entiende un poco mejor la logica:
-			//else{
-			//maximos[tid]=maximos[tid];
-			//indices[tid]=indices[tid]:
-			//}
-		}
-		__syncthreads (); // sincronización de los hilos de un bloque
-	}
-	if (tid == 0){
-		indicesBloques[blockIdx.x] = maximos[0];
-		maximosBloques[blockIdx.x] = indices[0];
-	}
-}
+// // Función para encontrar la palabra más cercana al vector resultante
+// void find_closest_word(float *result_vector, float *words, int numWords, int
+// idx1, int idx2, int idx3, int *closest_word_idx, float *max_similarity,int
+// numBloques,int numHilos) { 	int i,j; 	*max_similarity = 0.0;
+// *closest_word_idx = -1;	//copiamos el nuevo indice 	float
+// *maxBloques = (float*) calloc(numBloques, sizeof(float)); 	int * indices
+// =(int*)malloc(sizeof(int)*numBloques); 	int *cu_ind; 	float *
+// cu_max,*cu_words,*cu_vector_res;
+// 	//pasar los datos a el device:
+// 	//TODO
+// 	//crear vector maxBloques
+// 	cudaMalloc(&cu_max,numBloques*sizeof(float));
+// 	//crear y pasar vector indicesBloques
+// 	cudaMalloc(&cu_ind,numBloques*sizeof(int));
+// 	//crear y pasar result_vector
+// 	cudaMalloc(&cu_vector_res, EMB_SIZE*sizeof(float));
+// 	cudaMemcpy (cu_vector_res,result_vector,EMB_SIZE*sizeof(float),
+// cudaMemcpyHostToDevice);
+// 	//crear y pasar words
+// 	cudaMalloc(&cu_words,numWords*EMB_SIZE*sizeof(float));
+// 	cudaMemcpy (cu_words,words, numWords*EMB_SIZE*sizeof(float),
+// cudaMemcpyHostToDevice);
+//
+// 	// llamada a kernel:
+// 	reduc_analogy<<<numBloques, numHilos>>>(idx1, idx2, idx3, numWords,
+// cu_ind, cu_max, cu_words, cu_vector_res); reduc_analogy(int idx1,int idx2,int
+// idx3,int numWords,int * indicesBloques, float * maximosBloques,float *
+// words,float * result_vector)
+// 	//traerse los resultados del kernel:
+// 	cudaMemcpy(maxBloques, cu_max, numBloques*sizeof(float),
+// cudaMemcpyDeviceToHost); 	cudaMemcpy(indices, cu_ind,
+// numBloques*sizeof(int), cudaMemcpyDeviceToHost);
+// 	//procesar reslutados:
+// 	*max_similarity = -1.0;
+// 	for(i=0;i<numBloques;i++){
+// 		if (*max_similarity<maxBloques[i]){
+// 			*max_similarity = maxBloques[i];
+// 			*closest_word_idx = indices[i];
+// 		}
+// 	}
+//
+// 	//TODO algun free mas?
+// 	free(maxBloques);
+// 	free(indices);
+// 	cudaFree(cu_max);
+// 	cudaFree(cu_ind);
+// 	cudaFree(cu_vector_res);
+// 	cudaFree(cu_words);
+// 	return;
+// }
+//
+// __global__ void reduc_analogy(int idx1,int idx2,int idx3,int numWords,int *
+// indicesBloques, int * maximosBloques,float * words,float * result_vector)
+// {
+// 	int tid = threadIdx.x;
+// 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+// 	int i;
+// 	int stride=blockDim.x*gridDim.x;
+// 	int indice;
+// 	float simil;
+// 	float maxSimil=-1;
+// 	__shared__ int indices[blockDim.x];
+// 	__shared__ float maximos[blockDim.x];
+// 	for (i = idx;i<numWords;i+=stride){
+// 		simil =
+// cosine_similarity(result_vector,&words[i*EMB_SIZE],EMB_SIZE); if
+// (simil>maxSimil&& i !=idx1&& i !=idx2&& i !=idx3){ 			maxSimil
+// = simil; 			indice = i;
+// 		}
+// 	}
+// 	indices[tid]=indice;
+// 	maximos[tid]=maxSimil;
+// 	//ahora tenemos los mas grandes entre los que le ha tocado procesar a
+// este thread
+// 	__syncthreads ();
+// 	for (stride=1; stride<blockDim.x; stride*=2)
+// 	{
+// 		if ((tid % (2*stride)) == 0)
+// 		{
+// 			if(maximos[tid+stride]>maximos[tid]){
+// 				maximos[tid]=maximos[tid+stride];
+// 				indices[tid]=indices[tid+stride];
+// 			}
+// 			//esto no hace obviamente falta obviamente, pero lo dejo
+// porque se entiende un poco mejor la logica:
+// 			//else{
+// 			//maximos[tid]=maximos[tid];
+// 			//indices[tid]=indices[tid]:
+// 			//}
+// 		}
+// 		__syncthreads (); // sincronización de los hilos de un bloque
+// 	}
+// 	if (tid == 0){
+// 		indicesBloques[blockIdx.x] = maximos[0];
+// 		maximosBloques[blockIdx.x] = indices[0];
+// 	}
+// }
 
 //-------------------
 // KNN:
 //-------------------
 
-void knn_complet(float *words, int numwords, float *similarities) {
-	int i,j;
-	for(i = 0; i < numwords; i++){
-		for(j = 0; j < numwords; j++){
-			similarities[i*numwords+j] = cosine_similarity(&words[i*EMB_SIZE], &words[j*EMB_SIZE], EMB_SIZE);
-		}
-	}
+__global__ void knn_cuda(float *words, int *numwords, float *similarities) {
+  int tid = threadIdx.x;
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
+  int i, j;
+  int nwords = *numwords;
+
+  for (i = idx; i < nwords; i += stride) {
+    for (j = 0; j < nwords; j++) {
+      similarities[i * nwords + j] = cosine_similarity(
+          &words[i * EMB_SIZE], &words[j * EMB_SIZE], EMB_SIZE);
+    }
+  }
 }
+
+void knn_complet(float *words, int numwords, float *similarities) {
+  float *wordsCuda, *similaritiesCuda;
+  int *numwordsCuda;
+  int numBloques = 4, numHilos = 1024;
+
+  float Tex;
+  cudaEvent_t t0, t1;
+
+  cudaMalloc(&wordsCuda, numwords * EMB_SIZE * sizeof(float));
+  cudaMemcpy(wordsCuda, words, numwords * EMB_SIZE * sizeof(float),
+             cudaMemcpyHostToDevice);
+  cudaMalloc(&similaritiesCuda, numwords * numwords * sizeof(float));
+  cudaMemcpy(similaritiesCuda, similarities,
+             numwords * numwords * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMalloc(&numwordsCuda, sizeof(int));
+  cudaMemcpy(numwordsCuda, &numwords, sizeof(int), cudaMemcpyHostToDevice);
+
+  cudaEventCreate(&t0); // crear objeto
+  cudaEventCreate(&t1);
+  cudaEventRecord(t0); // tiempo en t
+
+  knn_cuda<<<numBloques, numHilos>>>(wordsCuda, numwordsCuda, similaritiesCuda);
+
+  cudaEventRecord(t1);      // tiempo en t1
+  cudaEventSynchronize(t1); // esperar hasta que
+  // t1 esté listo
+  cudaEventElapsedTime(&Tex, t0, t1); // calcula el tiempo
+  // entre t0 y t1
+  printf(" Tex (kernela): %fms\n", Tex); // milisegundos
+  cudaEventDestroy(t0);                  // borrar objeto
+  cudaEventDestroy(t1);
+
+  cudaMemcpy(words, wordsCuda, numwords * EMB_SIZE * sizeof(float),
+             cudaMemcpyDeviceToHost);
+  cudaMemcpy(similarities, similaritiesCuda,
+             numwords * numwords * sizeof(float), cudaMemcpyDeviceToHost);
+  cudaFree(wordsCuda);
+  cudaFree(similaritiesCuda);
+  cudaFree(numwordsCuda);
+}
+
 //-------------------
 // Debuging:
 //-------------------
-void imprimir_vector_float(float *vector, int tamano){
-	printf("\n");
-	for (int i = 0; i < EMB_SIZE; i++)	printf(" %2.3f",vector[i]);
-	printf("\n");
+void imprimir_vector_float(float *vector, int tamano) {
+  printf("\n");
+  for (int i = 0; i < EMB_SIZE; i++)
+    printf(" %2.3f", vector[i]);
+  printf("\n");
 }
-
--- cuda/knn_cuda.cu
+++ cuda/knn_cuda.cu
@@ -1,97 +1,105 @@
 
-// EXEC: knn embeddins.dat similarities.dat [numwords]   
+// EXEC: knn embeddins.dat similarities.dat [numwords]
 
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
-#include <time.h>
 #include "biblioteca_funciones.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-int main(int argc, char *argv[]) 
-{
-	int		i, j, numwords, row, col;
-	float 	*words;
-	FILE    	*f1, *f2;
-	float 	*similarities;
+int main(int argc, char *argv[]) {
+  int i, j, numwords;
+  float *words;
+  FILE *f1, *f2;
+  float *similarities;
 
-	struct timespec  t0, t1;
-	double tej;
+  struct timespec t0, t1;
+  double tej;
 
+  if (argc < 3) {
+    printf("Deia-Llamada: knn embeddins.dat similarities.dat [numwords]\n");
+    printf("\t(in) embeddings.dat\n");
+    printf("\t(out) similarities.dat\n");
+    printf("\t(hautazkoa) prozesatu nahi den hitz kopurua -- (opcional) num de "
+           "palabras que se quieren procesar\n");
+    exit(-1);
+    ;
+  }
 
-	if (argc < 3) {
-		printf("Deia-Llamada: knn embeddins.dat similarities.dat [numwords]\n");
-		printf("\t(in) embeddings.dat\n");
-		printf("\t(out) similarities.dat\n");
-		printf("\t(hautazkoa) prozesatu nahi den hitz kopurua -- (opcional) num de palabras que se quieren procesar\n");
-		exit (-1);;
-	}  
+  // Irakurri datuak sarrea-fitxategietatik
+  // ======================================
 
-	// Irakurri datuak sarrea-fitxategietatik
-	// ====================================== 
+  f1 = fopen(argv[1], "r");
+  if (f1 == NULL) {
+    printf("Errorea %s fitxategia irekitzean -- Error abriendo fichero\n",
+           argv[1]);
+    exit(-1);
+  }
 
-	f1 = fopen (argv[1], "r");
-	if (f1 == NULL) {
-		printf ("Errorea %s fitxategia irekitzean -- Error abriendo fichero\n", argv[1]);
-		exit (-1);
-	}
+  fscanf(f1, "%d", &numwords);
+  if (argc == 4)
+    numwords = atoi(argv[3]);
+  printf("numwords = %d\n", numwords);
 
-	fscanf (f1, "%d", &numwords);	       
-	if (argc == 4) numwords = atoi (argv[3]); 
-	printf ("numwords = %d\n", numwords);
+  /******************************************************************
+  // Asignar memoria dinámica a las estructuras de datos words y similarities
 
-	/******************************************************************
-	// Asignar memoria dinámica a las estructuras de datos words y similarities
+  //    OSATZEKO - PARA COMPLETAR
+   ******************************************************************/
 
-	//    OSATZEKO - PARA COMPLETAR
-	 ******************************************************************/
+  clock_gettime(CLOCK_REALTIME, &t0);
+  words = (float *)malloc(numwords * EMB_SIZE * sizeof(float));
+  similarities = (float *)malloc(numwords * numwords * sizeof(float));
 
-	words = (float *)malloc(numwords*EMB_SIZE*sizeof(float));
-	similarities = (float *)malloc(numwords*numwords*sizeof(float));
+  for (i = 0; i < numwords; i++) {
+    for (j = 0; j < EMB_SIZE; j++) {
+      fscanf(f1, "%f", &(words[i * EMB_SIZE + j]));
+    }
+  }
+  clock_gettime(CLOCK_REALTIME, &t1);
+  tej = (t1.tv_sec - t0.tv_sec) + (t1.tv_nsec - t0.tv_nsec) / (double)1e9;
+  printf("\n Lectura de datos = %1.3f ms\n\n", tej * 1000);
+  printf("Embeddingak irakurrita\n");
 
-	for (i=0; i<numwords; i++) {
-		for (j=0; j<EMB_SIZE; j++) {
-			fscanf (f1, "%f", &(words[i*EMB_SIZE+j]));
-		}
-	}
-	printf ("Embeddingak irakurrita\n");
+  printf("Hitz guztien auzokideak kalkulatzera zoaz\n");
+  clock_gettime(CLOCK_REALTIME, &t0);
+  /******************************************************************
+  // Deitu funtzioari
+  // Llamar a la función
 
-	printf("Hitz guztien auzokideak kalkulatzera zoaz\n");
-	clock_gettime (CLOCK_REALTIME, &t0);
-	/******************************************************************
-	// Deitu funtzioari
-	// Llamar a la función
+  //    OSATZEKO - PARA COMPLETAR
+   ******************************************************************/
+  knn_complet(words, numwords, similarities);
+  clock_gettime(CLOCK_REALTIME, &t1);
 
-	//    OSATZEKO - PARA COMPLETAR
-	 ******************************************************************/
-	knn_complet(words, numwords, similarities);
-	clock_gettime (CLOCK_REALTIME, &t1);
+  tej = (t1.tv_sec - t0.tv_sec) + (t1.tv_nsec - t0.tv_nsec) / (double)1e9;
+  printf("\n Tej. (serie) = %1.3f ms\n\n", tej * 1000);
 
-	tej = (t1.tv_sec - t0.tv_sec) + (t1.tv_nsec - t0.tv_nsec) / (double)1e9;
-	printf("\n Tej. (serie) = %1.3f ms\n\n", tej*1000);
+  // Idatzi antzekotasunak similarities fitxategietan -- Escribe las similitudes
+  // en el fichero similarities
+  f2 = fopen(argv[2], "w");
+  if (f2 == NULL) {
+    printf("Errorea %s fitxategia irekitzean -- Error abriendo fichero\n",
+           argv[2]);
+    exit(-1);
+  }
 
-	// Idatzi antzekotasunak similarities fitxategietan -- Escribe las similitudes en el fichero similarities
-	f2 = fopen (argv[2], "w");
-	if (f2 == NULL) {
-		printf ("Errorea %s fitxategia irekitzean -- Error abriendo fichero\n", argv[2]);
-		exit (-1);
-	}
+  fprintf(f2, "%d\n", numwords);
+  for (i = 0; i < numwords; i++) {
+    for (j = 0; j < numwords; j++) {
+      fprintf(f2, "%f\t", similarities[i * numwords + j]);
+    }
+    fprintf(f2, "\n");
+  }
+  printf("antzekotasunak idatzita fitxategietan -- similitudes escritas en el "
+         "fichero\n");
 
-	fprintf (f2, "%d\n", numwords);
-	for (i=0; i<numwords; i++) {
-		for (j=0; j<numwords; j++) {
-			fprintf (f2, "%f\t", similarities[i*numwords+j]);
-		} 
-		fprintf  (f2, "\n"); 
-	}
-	printf ("antzekotasunak idatzita fitxategietan -- similitudes escritas en el fichero\n");
+  fclose(f1);
+  fclose(f2);
 
-	fclose (f1);
-	fclose (f2);
+  free(words);
+  free(similarities);
 
-	free(words);
-	free(similarities); 
-
-	return 0;
+  return 0;
 }
-
